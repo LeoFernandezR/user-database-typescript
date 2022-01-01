702a34f59fc891dffda2801689c131ae3c5b21bf
Leonardo Fernández <leonardo.fernandez1402@gmail.com>
2021-12-31 22:27:53 -0300

    Rendering UserList, saving Users as a State, handleNewUser func

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,24 @@
-import React from "react"
+import React, {useState} from "react"
 
 import AddUser from "./components/Users/AddUser"
+import UserList from "./components/Users/UserList"
+import {User} from "./types"
+
 interface Props {}
 
 const App: React.FC<Props> = ({}) => {
+  const [users, setUsers] = useState<User[]>([])
+
+  const handleAddNewUser = (newUser: User) => {
+    setUsers((prevUsers) => {
+      return [...prevUsers, newUser]
+    })
+  }
+
   return (
     <div>
-      <AddUser />
+      <AddUser addNewUser={handleAddNewUser} />
+      <UserList users={users} />
     </div>
   )
 }
