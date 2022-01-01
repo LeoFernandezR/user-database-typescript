3515afb06927565a424fdd2f1b91f41a646d2bf1
Leonardo Fernández <leonardo.fernandez1402@gmail.com>
2022-01-01 20:01:36 -0300

    ErrorData state, conditional ErrorModal and cleanErrorData func

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,24 @@
 import React, {useState} from "react"
 
+import ErrorModal from "./components/UI/ErrorModal"
 import AddUser from "./components/Users/AddUser"
 import UserList from "./components/Users/UserList"
-import {User} from "./types"
+import {ErrorData, User} from "./types"
 
 interface Props {}
 
 const App: React.FC<Props> = ({}) => {
   const [users, setUsers] = useState<User[]>([])
+  const [error, setError] = useState<ErrorData>({
+    title: "",
+    message: "",
+  })
+
+  const hasBeenSentAnError = error.title.length > 0 && error.message.length > 0
+
+  const cleanErrorData = () => {
+    setError({title: "", message: ""})
+  }
 
   const handleAddNewUser = (newUser: User) => {
     setUsers((prevUsers) => {
@@ -17,7 +28,8 @@ const App: React.FC<Props> = ({}) => {
 
   return (
     <div>
-      <AddUser addNewUser={handleAddNewUser} />
+      {hasBeenSentAnError && <ErrorModal {...error} closeErrorModal={cleanErrorData} />}
+      <AddUser addNewUser={handleAddNewUser} showErrorModal={setError} />
       <UserList users={users} />
     </div>
   )
